df94ea363864e606f525fecc3575b4f2527abc7a
Lihan Li <frankieteardrop@gmail.com>
2015-07-30 12:18:24 -0400

    camelize function

--- dist/coding_inflector.js
+++ dist/coding_inflector.js
@@ -1,3 +1,21 @@
 (function() {
+  var STRING_UNDERSCORE_REGEXP_1 = (/([a-z\d])([A-Z]+)/g);
+  var STRING_UNDERSCORE_REGEXP_2 = (/\-|\s+/g);
+  var STRING_DASHERIZE_REGEXP = (/[ _]/g);
+  var STRING_DECAMELIZE_REGEXP = (/([a-z\d])([A-Z])/g);
+  var STRING_CAMELIZE_REGEXP = /[_.-](\w|$)/g;
 
+  var codingInflector = {
+    camelize: function(str) {
+      return str.replace(STRING_CAMELIZE_REGEXP, function(__, x) {
+        return x.toUpperCase();
+      });
+    },
+  };
+
+  if (typeof exports !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = codingInflector;
+  } else {
+    window.codingInflector = codingInflector;
+  }
 })();
---
+++ test/coding_inflector_test.js
@@ -0,0 +1,18 @@
+var expect = require("expect");
+var codingInflector = require('../dist/coding_inflector');
+
+var underscoreString = 'foo_bar';
+var camelizedString = 'fooBar';
+var dasherizedString = 'foo-bar';
+
+describe('coding_inflector', function() {
+  describe('#camelize()', function () {
+    it('should work on underscore strings', function () {
+      expect(codingInflector.camelize(underscoreString)).toBe(camelizedString);
+    });
+
+    it('should work on dasherized strings', function() {
+      expect(codingInflector.camelize(dasherizedString)).toBe(camelizedString);
+    });
+  });
+});
